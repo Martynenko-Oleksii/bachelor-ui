cedaf93de4130ebede24c1c78aec25e1464b97c7
Oleksii Martynenko <oleksii.martynenko@nure.ua>
2023-06-11 12:39:15 +0300

    Updated server data

--- app-ui/src/environments/environment.prod.ts
+++ app-ui/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
-    securityApi: 'http://localhost:8081/',
-    dataApi: 'http://localhost:8082/',
-    reportingApi: 'https://localhost:7004/api/',
+    securityApi: 'http://aranod.kh.ua:8081/',
+    dataApi: 'http://aranod.kh.ua:8082/',
+    reportingApi: 'http://aranod.kh.ua:5004/api/',
   
-    authority: 'http://localhost:5001',
-    redirectUrl: 'http://localhost:4200/home/',
-    postLogoutRedirectUri: 'http://localhost:4200/',
+    authority: 'http://aranod.kh.ua:5001',
+    redirectUrl: 'http://aranod.kh.ua/home/',
+    postLogoutRedirectUri: 'http://aranod.kh.ua/',
     clientId: 'spa',
     scope: 'openid profile roles customer additional security data reporting offline_access',
     responseType: 'code',
